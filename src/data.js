cdc54494856735a34a4c4548ff47c76fb412f246
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-06-20 12:16:55 -0500

    comenzando funcion para test

--- src/data.js
+++ src/data.js
@@ -1,7 +1,13 @@
 window.computeUserStats = (users, progress, courses) => {
-  // console.log(users);
-  // console.log(progress);
-  // console.log(courses);
+  let usersWithStats = [];
+  for (const user of users) {
+    for (const key of arrProgress) {
+      if (user.id === key && progress[key].hasOwnProperty('intro')) {
+        // console.log(progress[key].intro.percent);
+        // resultTable.innerHTML += "<tr><th scope='row'>" + user.name + "</th> <td>Intro</td> <td>" + progress[key].intro.percent +"%</td></tr>";
+      }
+    }
+  }
 }
 
 window.sortUsers = (users, orderBy, orderDirection) => {users, orderBy, orderDirection}
